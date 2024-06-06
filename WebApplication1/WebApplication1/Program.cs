557b5b9034bb2bf03478af9c90da357e2a3dc72c
s28876 <s28876@pjwstk.edu.pl>
2024-06-06 23:13:26 +0200

    fff

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -1,44 +1,90 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<TripsDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
+    
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseAuthorization();
 
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
+app.MapControllers();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+public class TripsDbContext : DbContext
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public TripsDbContext(DbContextOptions<TripsDbContext> options) : base(options) { }
+
+    public DbSet<Client> Clients { get; set; }
+    public DbSet<Trip> Trips { get; set; }
+    public DbSet<Country> Countries { get; set; }
+    public DbSet<TripCountry> TripCountries { get; set; }
+    public DbSet<ClientTrip> ClientTrips { get; set; }
+}
+
+public class Client
+{
+    public int IdClient { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Telephone { get; set; }
+    public string Pesel { get; set; }
+}
+
+public class Trip
+{
+    public int IdTrip { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime DateFrom { get; set; }
+    public DateTime DateTo { get; set; }
+    public int MaxPeople { get; set; }
+}
+
+public class Country
+{
+    public int IdCountry { get; set; }
+    public string Name { get; set; }
+}
+
+public class TripCountry
+{
+    public int IdTrip { get; set; }
+    public int IdCountry { get; set; }
+    public Trip Trip { get; set; }
+    public Country Country { get; set; }
+}
+
+public class ClientTrip
+{
+    public int IdClient { get; set; }
+    public int IdTrip { get; set; }
+    public DateTime RegisteredAt { get; set; }
+    public DateTime? PaymentDate { get; set; }
+    public Client Client { get; set; }
+    public Trip Trip { get; set; }
 }
